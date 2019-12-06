5ab7e4850da02ba00de2d2a534dce924f88af1d3
realprinceviper <idrisadeniji01@gmail.com>
2019-12-06 14:07:13 -0500

    create views directory

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ app.get('/', (req, res) => {
     res.send('Hello Express.');
 });
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public')); 
 
 app.get('/about', (req, res) => {
-    res.send('<h1>About Page</h1>');
+    res.render('about.hbs');
 });
 
 app.get('/bad', (req, res) => {
