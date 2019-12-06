65def37a83a1d13a119f7c0c1cf96e47933b6133
realprinceviper <idrisadeniji01@gmail.com>
2019-12-06 14:24:40 -0500

    create homepage with welcome message

--- server.js
+++ server.js
@@ -2,13 +2,20 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Express.');
+    res.render('home.hbs', {
+        pageTitle: 'Homepage',
+        welcomeMessage: 'Welcome to ATB Techsoft',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.use(express.static(__dirname + '/public')); 
 
 app.get('/about', (req, res) => {
-    res.render('about.hbs');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/bad', (req, res) => {
