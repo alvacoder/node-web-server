66fe0029a716cfeee8262539d9ee3816d028ffef
realprinceviper <idrisadeniji01@gmail.com>
2019-12-06 15:58:00 -0500

    saparate header and footer into partials

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const hbs = require('hbs');
+
 const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.use(express.static(__dirname + '/public')); 
 
 app.get('/', (req, res) => {
     res.render('home.hbs', {
         pageTitle: 'Homepage',
-        welcomeMessage: 'Welcome to ATB Techsoft',
-        currentYear: new Date().getFullYear()
+        welcomeMessage: 'Welcome to ATB Techsoft'
     });
 });
-
-app.use(express.static(__dirname + '/public')); 
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
