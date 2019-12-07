6d61d3384b392f561061f8dfce1d2741bf14d171
realprinceviper <idrisadeniji01@gmail.com>
2019-12-07 08:46:40 -0500

    write test for square function

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -5,5 +5,13 @@ it('should add two numbers', () => {
     if(res !== 44) {
         throw new Error(`returned ${res} instead of 44`);
     }
-    //console.log(res);
+    console.log(res);
 });
+
+it('should square a number', () => {
+    let res = utils.square(5);
+    if(res !== 25) {
+        throw new Error(`Expected 25 but got ${res}`);
+    }
+    console.log(res);
+})
